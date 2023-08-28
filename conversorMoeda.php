6d060b1858b28d0d62801d999e5b22b10d9a9ce5
TatianaAlves1 <tatipublisher@gmail.com>
2023-08-28 16:00:06 -0400

    Arquivos e conf iniciar conversor de moedas

---
+++ conversorMoeda.php
@@ -0,0 +1,20 @@
+<?php
+    require 'vendor/autoload.php';
+    class Conversor{
+        
+        private static $url = "http://economia.awesomeapi.com.br/json/last/USD-BRL"; 
+        
+        public static function verificarAcesso(){
+            
+        }
+        
+        public function mostrarValor($moeda){
+
+        }
+
+        public function ConverterValor($moeda,$valor){
+
+        }
+
+    }
+?>
